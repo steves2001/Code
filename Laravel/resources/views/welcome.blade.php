ab35342d7ffecc540df557dd58c2fac18e7fad7c
Stephen Smith <me@stephensmith.me.uk>
2017-05-29 07:20:26 +0000

    Testing install

--- Laravel/resources/views/welcome.blade.php
+++ Laravel/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+                <div>Stephens Test</div>
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
